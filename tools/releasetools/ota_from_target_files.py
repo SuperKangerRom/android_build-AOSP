3be21fd61c3b0dca4f056b19e9846ef5e6b4eb1a
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2015-12-04 20:59:10 +0100

    Flash SuperSU [1/2]
    Thanks to chainfire's "How to SU" guide,
    I've managed to get SuperSU zip flashing on every
    rom flash and it can easily be updated in
    vendor_vrtoxin
    
    Also Settings part merged in from OmniRom
    
    Change-Id: I0f57f8c2b8733021c3c0b9a5fd3c343ae565a959
    Signed-off-by: LorDClockaN <davor@losinj.com>
    
    VRToxin:
    Original commits by LorDClockaN
    Fix TWRP warning by reS28raM
    
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -684,6 +684,15 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     if block_based:
       script.Unmount("/system")
 
+  if block_based:
+    script.Print("Flashing SuperSU...")
+    common.ZipWriteStr(output_zip, "supersu/supersu.zip",
+                   ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
+    script.Mount("/system")
+    script.FlashSuperSU()
+  if block_based:
+    script.Unmount("/system")
+
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
@@ -692,6 +701,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.extra_script is not None:
     script.AppendExtra(OPTIONS.extra_script)
+
+  # SuperSU leave /system unmounted while we need it mounted here to avoid
+  # a warning from non-Multirom TWRP
+  if block_based:
+    script.Mount("/system")
 
   script.UnmountAll()
 
